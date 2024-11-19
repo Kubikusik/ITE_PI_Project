1089d06dbf3459c77c15737fc762dd815888802a
Kubikusik <jakubmical@student.agh.edu.pl>
2024-11-19 17:18:20 +0100

    Restructuring
    Removed Physics from ConwaysGameOFLifeMaksiowy and moved it into separate file (so it makes more sense)

---
+++ RoadSystems/ConwayBasedPhysics.hpp
@@ -0,0 +1,10 @@
+#ifndef CONWAYBASEDPHYSICS_H
+#define CONWAYBASEDPHYSICS_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "UIElements.h"
+#pragma once
+void ConwaysPhysics(Grid_Tiles**& grid_list, int grid_num, std::vector<sf::Color>& color_list);
+#endif
+#pragma once
